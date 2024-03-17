7fc5fa69673f5ea9721b984c2cc396bfc2c92af7
Hitesh Natha <93398146+hiteshnatha15@users.noreply.github.com>
2024-03-17 19:48:25 +0530

    Delete WEB DEVELPOMENT directory

--- WEB DEVELPOMENT/COUNTER/index.js
+++
@@ -1,9 +0,0 @@
-let value = document.getElementById("value");
-console.log(value.innerText);
-
-document.getElementById("plus").addEventListener("click", function () {
-  value.innerText = parseInt(value.innerText) + 1;
-});
-document.getElementById("minus").addEventListener("click", function () {
-  value.innerText = parseInt(value.innerText) - 1;
-});
--- WEB DEVELPOMENT/DEV DETECTIVE/script.js
+++
@@ -1,8 +0,0 @@
-const themeBtn = document.querySelector("#btn-mode");
-const modeText = document.querySelector("#mode-text");
-const modeIcon = document.querySelector("#mode-icon");
-
-themeBtn.addEventListener("click", () => {
-  modeText.textContent = "LIGHT";
-  modeIcon.src = "./assets/images/sun-icon.svg";
-});
